bd50373cbdccaa3e164cfd98c6756f12fbff6b2e
Avantika Mahalingam <avantilingam@gmail.com>
2023-04-30 21:32:57 -0400

    label update

--- chat-streamlit.py
+++ chat-streamlit.py
@@ -46,8 +46,8 @@ if previous_messages:
     st.write("Chat Log:")
     for message in previous_messages:
         if message.startswith("You:"):
-            st.text_input(label = " ", value=message, key=message)
+            st.text_input(label = "input", value=message, key=message,label_visibility="hidden")
         else:
             num_lines = len(message.split("\n"))
             height = min(300, max(30, num_lines * 25))
-            st.text_area(label = " ", value=message, height=height, key=message)
+            st.text_area(label = "input", value=message, height=height, key=message,label_visibility="hidden")
